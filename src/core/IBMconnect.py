945cc2ea08c71dda7f839d704310ae25853b7d83
Luan Brito <lab7@cin.ufpe.br>
2019-06-06 21:14:45 +0000

    basic core example with mock audio works

--- src/core/IBMconnect.py
+++ src/core/IBMconnect.py
@@ -16,19 +16,26 @@ def audio_to_text(filePath):
         result = speech_to_text.recognize(
             audio_file, content_type='audio/wav', timestamps=True, model="pt-BR_NarrowbandModel",
             word_confidence=True, profanity_filer=False).get_result()
-        if (result["error"]):
+        print(result)
+        if ("error" in result.keys()):
             return result["code_description"]
         else:
-            return result['results'][0]['alternatives'][0]['timestamps']
+            return result['results']
 
-blockedWords = {'estando': True, 'mano': True}
-audio = AudioSegment.from_wav("test-pt.wav")
+blockedWords = { 'cu': True, 'porra': True } # Mock
 
-timestamps = audio_to_text('./test-pt.wav')
-print(timestamps)
+audio = AudioSegment.from_wav("test-pt.wav") # original audio
+timestamps = [] # Timestamps
+response = audio_to_text('./test-pt.wav')
+for i in response:
+    for j in (i['alternatives'][0]['timestamps']):
+        timestamps.append(j)
 secs = 1000
-edited = []
+edited = AudioSegment.empty()
+for i in timestamps:
+    print(i)
+    if not i[0] in blockedWords.keys():
+        edited += audio[i[1] * secs : i[2] * secs] # Concat with chunck that is not blocked
 
-# for i in timestamps:
-#     if not i[0] in blockedWords.keys():
-#         edited.append(audio[i[1] * secs : i[2] * secs])
+print(edited) # final audio 
+edited.export("./result.wav", format="wav")
